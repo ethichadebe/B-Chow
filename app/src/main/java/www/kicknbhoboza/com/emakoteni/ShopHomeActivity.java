9a9141e419045cdf9f75677ebae10721d98e2927
Ethic Hadebe <ethichadebe@gmail.com>
2020-01-20 15:29:14 +0200

    Updates

--- app/src/main/java/www/kicknbhoboza/com/emakoteni/ShopHomeActivity.java
+++ app/src/main/java/www/kicknbhoboza/com/emakoteni/ShopHomeActivity.java
@@ -1,14 +1,27 @@
 package www.kicknbhoboza.com.emakoteni;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ShopHomeActivity extends AppCompatActivity {
 
+    CardView cvKota;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_home);
+
+        cvKota = findViewById(R.id.cvKota);
+
+        cvKota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ShopHomeActivity.this, OrderActivity.class));
+            }
+        });
     }
 }
