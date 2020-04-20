4f94c62338f17a5a31c983c96c39bd05f97ef50d
Ethic Hadebe <ethichadebe@gmail.com>
2020-04-20 19:22:15 +0200

    Chips always checked

--- app/src/main/java/Adapter/IngredientItemCheckboxAdapter.java
+++ app/src/main/java/Adapter/IngredientItemCheckboxAdapter.java
@@ -77,7 +77,7 @@ public class IngredientItemCheckboxAdapter extends RecyclerView.Adapter<Ingredie
         }
 
         if (holder.tvIngredientName.getText().toString().toLowerCase().equals("chips")) {
-            holder.tvIngredientName.setOnClickListener(view -> holder.tvIngredientName.setChecked(true));
+            holder.tvIngredientName.setChecked(true);
             holder.tvIngredientName.setClickable(false);
         }
     }
